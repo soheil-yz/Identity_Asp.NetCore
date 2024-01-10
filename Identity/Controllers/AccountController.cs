f075827f324c423dc3e1cd1043d7c8795502e135
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-10 23:52:34 +0330

    Create Account Controller  And CreateView for Register

---
+++ Identity/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Identity.Models.Entities.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            return View();
+        }
+    }
+}
--- Identity/Models/Entities/Dto/RegisterDto.cs
+++ Identity/Models/Entities/Dto/RegisterDto.cs
@@ -1,6 +1,25 @@
-﻿namespace Identity.Models.Entities.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.Entities.Dto
 {
     public class RegisterDto
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string EmailConfirmed { get; set; }
     }
 }
