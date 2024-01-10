cee02668511d24f5fe955d88630ed4f0292d7557
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-10 22:59:23 +0330

    Activate Entity Ignore

--- Identity/Data/DataBaseContext.cs
+++ Identity/Data/DataBaseContext.cs
@@ -5,10 +5,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Data
 {
-    public class DataBaseContext : IdentityDbContext<Users,Roles,string>
+    public class DataBaseContext : IdentityDbContext<Users, Roles, string>
     {
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { 
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
         }
 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserLogin<string>>().HasKey(p => new
+            { p.ProviderKey, p.LoginProvider });
+            builder.Entity<IdentityUserRole<string>>().HasKey(p => new
+            { p.UserId, p.RoleId});            
+            builder.Entity<IdentityUserToken<string>>().HasKey(p => new
+            { p.UserId, p.LoginProvider , p.Name});
+
+            builder.Entity<Users>().Ignore(p=> p.NormalizedEmail);
+            //base.OnModelCreating(builder);  
+        }
     }
 }
