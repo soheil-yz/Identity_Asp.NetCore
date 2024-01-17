c67b8fbf9775c6591e627e47a2578ed92ad340da
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-17 12:40:49 +0330

    Create Admin Area

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -34,4 +34,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+  name: "areas",
+  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
 app.Run();
