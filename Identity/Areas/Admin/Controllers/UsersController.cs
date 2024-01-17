763727a8e6c30eda8585d2b5d6e10ffa7e072810
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-17 18:04:07 +0330

    Enable Register Item for Admin

---
+++ Identity/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Identity.Areas.Admin.Models.Dto;
+using Identity.Models.Entities;
+using Identity.Models.Entities.Dto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+        public UsersController(UserManager<Users> userManager)
+        {
+            _userManager = userManager; 
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.Select(p => new UserListDto
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                UserName = p.UserName,
+                PhoneNumber = p.PhoneNumber,
+                EmailConfirmed = p.EmailConfirmed,
+                AccessFailedCount = p.AccessFailedCount
+
+            }).ToList();
+            return View(users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(RegisterDto registerDto)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(registerDto);
+            }
+            Users newUser = new Users
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+
+            };
+
+            var Users = _userManager.CreateAsync(newUser, registerDto.Password).Result;
+            if (Users.Succeeded)
+                return RedirectToAction("Index", "Home");
+
+            string message = "";
+            foreach (var item in Users.Errors.ToList())
+                message += item.Description + Environment.NewLine;
+
+            TempData["Massege"] = message;
+            return View(registerDto);
+        }
+    }
+    
+}
---
+++ Identity/Areas/Admin/Models/Dto/UserListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Identity.Areas.Admin.Models.Dto
+{
+    public class UserListDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public int AccessFailedCount { get; set; }
+    }
+}
