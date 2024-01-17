ab75eb0ac5eed6aa40d098778d8fd4c9bc51c02a
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-18 02:39:39 +0330

    Create rolesControllers

---
+++ Identity/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Areas.Admin.Models.Dto.Roles;
+using Identity.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<Roles> _roleManager;
+
+        public RolesController(RoleManager<Roles> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var role = _roleManager.Roles.Select(p=> 
+            new RolesListDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+            }).ToList();
+            return View(role);
+        }
+    }
+}
---
+++ Identity/Areas/Admin/Models/Dto/Roles/AddNewRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Areas.Admin.Models.Dto.Roles
+{
+    public class AddNewRoleDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
