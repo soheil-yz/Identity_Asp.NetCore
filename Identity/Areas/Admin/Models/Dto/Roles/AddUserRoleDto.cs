af598fe620d988f77113a59f443fd28426ec3cf0
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-01-31 03:46:26 +0330

    Add AddUserRoles Model

---
+++ Identity/Areas/Admin/Models/Dto/Roles/AddUserRoleDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Identity.Areas.Admin.Models.Dto.Roles
+{
+    public class AddUserRoleDto
+    {
+        public string Id { get; set; }
+        public string Role {  get; set; }
+        public List<SelectListItem> Roles { get; set; }
+    }
+}
