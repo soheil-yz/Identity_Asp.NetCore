88d5b7c17551773feadda7b912f6b35cfcd425e8
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-02-02 11:42:38 +0330

    Enable Email Service

--- Identity/Services/EmailService.cs
+++ Identity/Services/EmailService.cs
@@ -1,6 +1,29 @@
-﻿namespace Identity.Services
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Identity.Services
 {
     public class EmailService
     {
+        public Task Execute(string UserEmail, string Body, string Subject)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 1000000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("soheilyazdanii1382@gmail.com", "password");
+            MailMessage message = new MailMessage("soheilyazdanii1382@gmail.com", UserEmail, Subject, Body);
+            message.IsBodyHtml = true;
+            message.BodyEncoding = UTF8Encoding.UTF8;
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;    
+
+            client.Send(message);
+            return Task.CompletedTask;
+
+        }
     }
 }
