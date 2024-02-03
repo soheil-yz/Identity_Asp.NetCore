0c549a93360677f0e022d160869cf24c961e6f15
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-02-03 11:30:56 +0330

    Create ConfirmEmail method in Account class

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -66,8 +66,20 @@ namespace Identity.Controllers
 
         public IActionResult ConfirmEmail(string UserId , string Token)
         {
+            if (UserId == null || Token == null)
+            {
+                return BadRequest();
+            }
+            var user = _userManager.FindByIdAsync(UserId).Result;
+            if (user == null)
+                return View("Error");
+            var result = _userManager.ConfirmEmailAsync(user, Token).Result;
+            if (result.Succeeded)
+            {
 
-            return View();
+            }
+
+            return RedirectToAction("login");
         }
         public IActionResult DisplayEmail()
         {
